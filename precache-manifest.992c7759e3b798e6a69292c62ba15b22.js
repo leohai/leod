b4e7f4e3b719aa7299f3e42fcc8c81621c5b1fd3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-14 05:00:07 +0000

    Deploy leohai/leod to github.com/leohai/leod.git:gh-pages

---
+++ precache-manifest.992c7759e3b798e6a69292c62ba15b22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b469b47844089ef56a26709c9dc5d8c6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ef929c9685ec725cea17.bundle.js"
+  },
+  {
+    "url": "runtime~main.ef929c9685ec725cea17.bundle.js"
+  },
+  {
+    "revision": "5d6f1ad25ca951dc2ea1",
+    "url": "static/css/main.14450879.chunk.css"
+  },
+  {
+    "url": "vendors~main.ef929c9685ec725cea17.bundle.js"
+  },
+  {
+    "url": "vendors~main.ef929c9685ec725cea17.bundle.js.LICENSE.txt"
+  }
+]);
