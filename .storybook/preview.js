825c3936d97f0a50b025c584d46b9e4de58b606c
leohai <448456376@qq.com>
2020-05-14 11:11:46 +0800

    add travis ci

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,24 @@
 import '../src/styles/index.scss'
-import { addDecorator } from '@storybook/react'
+import { addDecorator, configure } from '@storybook/react'
 import React from 'react'
+
 const wrapper = {
   padding: '20px 40px'
 }
+const loaderFn = () => {
+  const allExports = [require('../src/welcome.stories.tsx')];
+  const req = require.context('../src/components', true, /\.stories\.tsx$/);
+  req.keys().forEach(fname => allExports.push(req(fname)));
+  return allExports;
+};
+configure(loaderFn, module);
+
 addDecorator(storyFn =>
   <div style={wrapper}>
     <h3>组件演示</h3>
     {storyFn()}
-  </div>)
-
+  </div>
+)
 /** addon-info 自动补全文档过时 */
 // import { addParameters } from '@storybook/react'
 // import { withInfo } from '@storybook/addon-info'
