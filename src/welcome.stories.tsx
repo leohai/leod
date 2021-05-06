e4fd35e42e06e857f61c4b71ddacc1edb07b1886
liuhai <liuhai@qinsilk.com>
2021-05-06 19:54:28 +0800

    style: update learning document

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -7,7 +7,7 @@ storiesOf('Welcome page', module)
       <>
         <h1>欢迎来到 leod 组件库</h1>
         <p>leod 是一套Typescript + react 的组件样式库</p>
-        <h3>安装试试</h3>
+        <h3>安装试试鸭~</h3>
         <code>
           npm install leod --save
         </code>
